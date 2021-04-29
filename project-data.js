05fb194e8e0559554e032b567db2fe0ccdb90574
Zil Norvilis <zilasino27@gmail.com>
2021-04-29 17:40:57 +0200

    Add js for  setting projects title, descr. and img

---
+++ project-data.js
@@ -0,0 +1,15 @@
+export default function ProjectData() {
+        let project_data = { 1: {
+            title: 'Gift Grouping App',
+            tags: ['HTML/CSS', 'Bootstrap'],
+            image: 'grouping-app-screenshot.png',
+            description: 'Gift Grouping is a mobile-only app where you can sign up (and/or sign in), create groups and add gifts. View created groups created by all users and view only signed in user\'s added gifts.',
+            live_link: 'https://grouping-app.herokuapp.com/',
+            source_link: 'https://github.com/zilton7/grouping-app'
+        },
+        project_2: {
+
+        }
+    }
+    return project_data;
+}
--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-  // Get the modal
-  var modal = document.getElementById("myModal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function launchModal(){
-  // Get the button that opens the modal
-  // var btn = document.getElementById("myBtn");
-
-  // When the user clicks on the button, open the modal
-  modal.style.display = "block";
-}
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -22,4 +14,38 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} 
+}
+
+function launchModal(project){
+  // When the user clicks on the button, open the modal
+  console.log(project_data[project].title);
+  setProjectData(project_data[project]);
+  modal.style.display = "block";
+}
+
+function setProjectData(data){
+  // Set project title
+  document.getElementById("project-title").innerHTML = data.title;
+  document.getElementById("project-image").src = './assets/' + data.image;
+  document.getElementById("project-description").innerHTML = data.description;
+  
+}
+
+
+let project_data = { 1: {
+  title: 'Gift Grouping App',
+  tags: ['HTML/CSS', 'Bootstrap'],
+  image: 'grouping-app-screenshot.png',
+  description: 'Gift Grouping is a mobile-only app where you can sign up (and/or sign in), create groups and add gifts. View created groups created by all users and view only signed in user\'s added gifts.',
+  live_link: 'https://grouping-app.herokuapp.com/',
+  source_link: 'https://github.com/zilton7/grouping-app'
+  },
+  2: {
+    title: 'Online Book Store Design',
+    tags: ['HTML/CSS', 'Bootstrap'],
+    image: 'online-shop-screenshot.png',
+    description: 'Online shop design built with Bootstrap and custom HTML/CSS',
+    live_link: 'https://zilton7.github.io/online-shop/',
+    source_link: 'https://github.com/zilton7/online-shop'
+    }
+}
