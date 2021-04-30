57dd6ab5b26991e64b8f3bdd2b994b75aa17a6bf
Zil Norvilis <zilasino27@gmail.com>
2021-04-30 09:19:27 +0200

    Add all functionality for project modal

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  resetProjectData();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    resetProjectData();
   }
 }
 
@@ -26,9 +28,38 @@ function launchModal(project){
 function setProjectData(data){
   // Set project title
   document.getElementById("project-title").innerHTML = data.title;
+  insertTags(data.tags, document.getElementById('project-tags'));
   document.getElementById("project-image").src = './assets/' + data.image;
   document.getElementById("project-description").innerHTML = data.description;
+  assignLinks(data.live_link, data.source_link);
   
+}
+
+function insertTags(arr, target){
+  arr.forEach(el => {
+    target.innerHTML += '<span class="tag">' + el + '</span>';
+  });
+}
+
+function resetProjectData(){
+  document.getElementById("project-title").innerHTML = '';
+  document.getElementById('project-tags').innerHTML = '';
+  document.getElementById("project-image").src = '';
+  document.getElementById("project-description").innerHTML = '';
+  document.getElementById('project-links').innerHTML = '';
+}
+
+function assignLinks(live_link, source_link){
+  let links = [];
+  if(live_link!=''){
+    links.push('<span class="link-with-icon"><a href="'+ live_link +'" target="_blank" id="project-live-link">See live<i class="fas fa-external-link-alt"></i></a></span>');
+  };
+  if(source_link!=''){
+    links.push('<span class="link-with-icon"><a href="'+ source_link +'" target="_blank" id="project-source-link">Check source<i class="fab fa-github-square"></i></a></span>');
+  };
+  links.forEach(el => {
+    document.getElementById('project-links').innerHTML +=  el;
+  });
 }
 
 
