f31335271011dcdb597346c79dd90a3a55f4a42c
Zil Norvilis <zilasino27@gmail.com>
2021-07-31 12:15:05 +0200

    Add Text api functionality

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :projects, only: [:index] 
+      resources :projects, only: [:index]
+      resources :texts, only: [:index]
     end
   end
 end
