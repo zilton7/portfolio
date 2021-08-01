56e0930ca796edac8770278c9698d5a3538ca639
Zil Norvilis <zilasino27@gmail.com>
2021-08-01 12:00:51 +0200

    Implement ProjectDetail modal

--- app/javascript/components/ProjectDetail.js
+++ app/javascript/components/ProjectDetail.js
@@ -9,31 +9,69 @@ const ProjectDetail = ({ project }) => {
       history.push("/");
     }
   };
-  console.log("i am here");
+
   return (
+    // <div
+    //   className="project-detail-shadow close-project-detail"
+    //   role="button"
+    //   tabIndex="0"
+    //   onClick={exitDetailHandler}
+    //   onKeyDown={exitDetailHandler}
+    // >
+    //   <div className="project-detail">
+    //     <div className="project-detail-head">
+    //       <span
+    //         className="close-project-detail"
+    //         role="button"
+    //         tabIndex="0"
+    //         onClick={exitDetailHandler}
+    //         onKeyDown={exitDetailHandler}
+    //       >
+    //         X
+    //       </span>
+    //     </div>
+    //     <div className="project-detail-wrapper">
+    //       <h3 className="project-detail-name">{project.name}</h3>
+    //       <img src={project.image} alt="" />
+    //     </div>
+    //   </div>
+    // </div>
     <div
-      className="project-detail-shadow close-project-detail"
-      role="button"
-      tabIndex="0"
+      id="project-modal"
+      className="modal close-project-detail"
       onClick={exitDetailHandler}
       onKeyDown={exitDetailHandler}
     >
-      <div className="project-detail">
-        <div className="project-detail-head">
+      <div className="modal-content">
+        <div className="modal-head">
           <span
-            className="close-project-detail"
-            role="button"
-            tabIndex="0"
+            className="close close-project-detail"
             onClick={exitDetailHandler}
             onKeyDown={exitDetailHandler}
           >
-            X
+            &times;
           </span>
+          <p className="modal-title" id="project-title"></p>
         </div>
-        <div className="project-detail-wrapper">
-          <h3 className="project-detail-name">{project.name}</h3>
-          <img src="" alt="" />
+        <div className="project-tags" id="project-tags"></div>
+        <div className="modal-image-wrapper">
+          <div id="arrow-prev" className="arrow">
+            <i className="fas fa-arrow-circle-left"></i>
+          </div>
+          <div className="modal-image">
+            <img src="" id="project-image" alt="" />
+          </div>
+          <div id="arrow-next" className="arrow">
+            <i className="fas fa-arrow-circle-right"></i>
+          </div>
         </div>
+        <p className="description modal-description" id="project-description">
+          {project.description}
+        </p>
+        <div
+          className="link-with-icon-wrapper modal-link-wrapper"
+          id="project-links"
+        ></div>
       </div>
     </div>
   );
