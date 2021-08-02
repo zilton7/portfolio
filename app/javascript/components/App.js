cee5a13b930b762fd983e2b9beea98e05002a418
Zil Norvilis <zilasino27@gmail.com>
2021-08-02 09:29:53 +0200

    Add google analytics package

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import Home from "./Home";
 
+const [analytics, setAnalytics] = useState("");
+
 const App = () => {
+  if (analytics !== "") {
+    useEffect(() => {
+      ReactGA.initialize("Your Unique ID");
+    });
+  }
+
   return (
     <>
       <Route path={["/project/:id", "/"]}>
--- app/javascript/components/Portfolio.js
+++ app/javascript/components/Portfolio.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Project from "./Project";
 
-const Portfolio = ({ projects }) => {
+const Portfolio = ({ title, projects }) => {
   const projectList = projects.map((project) => <Project project={project} />);
 
   return (
     <div id="portfolio" className="section portfolio">
       <div className="portfolio-title-wrapper">
         <div className="number number-portfolio">02</div>
-        <h3 className="title">Portfolio</h3>
+        <h3 className="title">{title}</h3>
       </div>
       <div className="projects">{projectList}</div>
     </div>
