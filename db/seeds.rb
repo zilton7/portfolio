77bc23141c79f2194b49b2bac32d85dd253c1462
Zil Norvilis <zilasino27@gmail.com>
2021-08-03 16:49:15 +0200

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+case Rails.env
+when "development"
+   Admin.create(email: 'zil@norvilis.com', password: '123456', password_confirmation: '123456')
+  p 'Admin created'
+end
+
+
 
 projects = [
   {
